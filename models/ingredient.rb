7e8fccfa5eb1902ba1f08ad43dac179707b2a6b8
kms617 <krysia.codes@gmail.com>
2014-09-06 19:54:51 -0400

    done

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,42 +1,14 @@
 class Ingredient
-# attr_accessor :ingredients
+  attr_reader :name
 
-# def initialize
-#   @ingredients = []
+  def initialize(result)
+    @name = result["ingredients"]
+  end
 
 
-# def self.db_connection
-#     begin
-#       connection = PG.connect(dbname: 'recipes')
-
-#       yield(connection)
-
-#     ensure
-#       connection.close
-#     end
-#   end
-
-#   def ingredients.all
-#      query =%Q{SELECT * FROM recipe_info
-#                   WHERE id = $1;
-#                   }
-
-#          results = db_connection do |conn|
-#            conn.exec(query, [id])
-#          end
-
-#        results.to_a
-#      end
-#   end
-
-#   def get_ingredients(recipe)
-#       ingredients.each do |ingredient|
-#       # - find account that transaction belongs to
-#       recipe = recipes.find { |recipe| recipe.id == ingredients.recipe_id }
-#       # - push transaction into that account's @transactions array
-#       recipe.ingredients << ingredient
-#       end
-#     recipe.ingredients
-#   end
 
 end
+
+
+# self.find { |recipe| recipe.id == ingredients.recipe_id }
+#       recipe.ingredients << ingredient
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe
-  attr_reader :description, :instructions, :name, :id
+  attr_reader :name, :id
   attr_accessor :ingredients
 
   def initialize(result)
@@ -36,5 +36,54 @@ class Recipe
   end
 
   def self.find(id)
+    query = %Q{SELECT * FROM recipes WHERE id = $1;}
+
+    results = db_connection do |conn|
+      conn.exec(query, [id])
+    end
+    results = results.to_a
+    Recipe.new(results[0])
+
   end
+
+  def description
+   @description
+    if @description == nil
+    "This recipe doesn't have a description."
+    else
+    @description
+    end
+  end
+
+  def instructions
+   @instructions
+    if @instructions == nil
+    "This recipe doesn't have any instructions."
+    else
+    @instructions
+    end
+  end
+
+  def ingredients
+    query= %Q{SELECT ingredients.name AS ingredients
+              FROM ingredients
+              WHERE recipe_id = $1;}
+
+    results = Recipe.db_connection do |conn|
+    conn.exec(query, [id])
+    end
+    results = results.to_a
+    all_ingredients = []
+    results.each do |result|
+      all_ingredients << Ingredient.new(result)
+    end
+
+    all_ingredients
+  end
+
+# bank_data.each do |row|
+#   account = accounts.find {|account| row[:account] == account.name}
+#   account.transactions << Transaction.new(row[:date], row[:amount], row[:description])
+# end
+  # end
 end
