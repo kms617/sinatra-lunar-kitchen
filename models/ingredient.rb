a0c7d93572d84ad89ea0b129267d101633cd0223
kms617 <krysia.codes@gmail.com>
2014-09-05 21:06:18 -0400

    stopping for the night

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,2 +1,42 @@
 class Ingredient
+# attr_accessor :ingredients
+
+# def initialize
+#   @ingredients = []
+
+
+# def self.db_connection
+#     begin
+#       connection = PG.connect(dbname: 'recipes')
+
+#       yield(connection)
+
+#     ensure
+#       connection.close
+#     end
+#   end
+
+#   def ingredients.all
+#      query =%Q{SELECT * FROM recipe_info
+#                   WHERE id = $1;
+#                   }
+
+#          results = db_connection do |conn|
+#            conn.exec(query, [id])
+#          end
+
+#        results.to_a
+#      end
+#   end
+
+#   def get_ingredients(recipe)
+#       ingredients.each do |ingredient|
+#       # - find account that transaction belongs to
+#       recipe = recipes.find { |recipe| recipe.id == ingredients.recipe_id }
+#       # - push transaction into that account's @transactions array
+#       recipe.ingredients << ingredient
+#       end
+#     recipe.ingredients
+#   end
+
 end
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,40 @@
 class Recipe
+  attr_reader :description, :instructions, :name, :id
+  attr_accessor :ingredients
+
+  def initialize(result)
+    @id = result["id"]
+    @name = result["name"]
+    @description = result["description"]
+    @instructions = result["instructions"]
+    @ingredients = []
+  end
+
+  def self.db_connection
+    begin
+      connection = PG.connect(dbname: 'recipes')
+
+      yield(connection)
+
+    ensure
+      connection.close
+    end
+  end
+
+  def self.all
+    query= %Q{SELECT * FROM recipes ORDER BY name;}
+
+    results = db_connection do |conn|
+      conn.exec('SELECT * FROM recipes ORDER BY name;')
+    end
+
+    all_recipes = []
+    results.each do |result|
+      all_recipes << Recipe.new(result)
+    end
+    all_recipes
+  end
+
+  def self.find(id)
+  end
 end
